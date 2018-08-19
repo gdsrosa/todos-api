1a78c628d2752c25d8f1264f0a0405e8a2e8b1d5
gdsrosa <gabrieldsrosa@gmail.com>
2018-08-18 22:27:58 -0300

    Remove comment

--- todos/tests.py
+++ todos/tests.py
@@ -1,12 +1,12 @@
 from django.test import TestCase
 from todos.models import Todo
 
-#Model Test
+
 class TodoTest(TestCase):
-    
+
     def create_todo(self, title='Go to workshop', body='Learn with Python Workshop', done=False):
         return Todo.objects.create(title=title, body=body, done=done)
-    
+
     def test_create_todo(self):
         todo = self.create_todo()
         self.assertTrue(isinstance(todo, Todo))
