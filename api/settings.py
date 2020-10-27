0ad74afab81ef40ac184bf56ee31e5e8e048af99
Matheus Rosa <1862239+mdsrosa@users.noreply.github.com>
2020-10-27 18:30:19 +0000

    Protect SECRET_KEY under .env

--- api/settings.py
+++ api/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
+
+# Load env vars
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "(^t3vuq6j5%xmd)a#fy+-=zq)-btm757ddnwl55b_%4+xb7wz&"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
